f1d36883a6ece3ade73086e7e40fcf81efbca15b
Shantesh <shanthu1804@gmail.com>
2024-02-23 02:10:53 +0530

    apache camel route to Consume activemq messages, initial commit

---
+++ src/main/java/com/shantesh/camelmicroserviceb/CamelMicroserviceBApplication.java
@@ -0,0 +1,14 @@
+package com.shantesh.camelmicroserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceBApplication.class, args);
+		System.out.println("Learning Second route i.e, activemq................");
+	}
+
+}
